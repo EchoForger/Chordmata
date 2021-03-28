ddd399f5fac7553f20024c1c2871f788fcabe762
EchoForger <1621490592@qq.com>
2021-03-28 12:12:23 +0800

    forst

---
+++ build/src/CMakeFiles/Chordmata.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/Chordmata"
+  "../bin/Chordmata.pdb"
+  "CMakeFiles/Chordmata.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chordmata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
