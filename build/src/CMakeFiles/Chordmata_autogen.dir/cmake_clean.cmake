3c0d0ad227edc763a45611a3404f71b0a3f7e8d1
EchoForger <1621490592@qq.com>
2021-03-28 13:41:05 +0800

    s

--- build/src/CMakeFiles/Chordmata_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Chordmata_autogen"
-  "Chordmata_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Chordmata_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
