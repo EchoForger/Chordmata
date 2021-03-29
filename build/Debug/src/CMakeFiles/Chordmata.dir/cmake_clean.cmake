8260da75bc6739f4591e2f363dd66f7e5f6bf8f7
EchoForger <1621490592@qq.com>
2021-03-29 12:56:31 +0800

    s

---
+++ build/Debug/src/CMakeFiles/Chordmata.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "../../../src/Chordmata_en_US.ts"
+  "../bin/Chordmata"
+  "../bin/Chordmata.pdb"
+  "CMakeFiles/Chordmata_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Chordmata_autogen.dir/ParseCache.txt"
+  "Chordmata_autogen"
+  "CMakeFiles/Chordmata.dir/Chordmata_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Chordmata.dir/chord.cpp.o"
+  "CMakeFiles/Chordmata.dir/clip.cpp.o"
+  "CMakeFiles/Chordmata.dir/main.cpp.o"
+  "CMakeFiles/Chordmata.dir/mainwindow.cpp.o"
+  "CMakeFiles/Chordmata.dir/midiclip.cpp.o"
+  "CMakeFiles/Chordmata.dir/midievent.cpp.o"
+  "CMakeFiles/Chordmata.dir/miditrack.cpp.o"
+  "CMakeFiles/Chordmata.dir/mixer.cpp.o"
+  "CMakeFiles/Chordmata.dir/pianowork.cpp.o"
+  "CMakeFiles/Chordmata.dir/project.cpp.o"
+  "CMakeFiles/Chordmata.dir/track.cpp.o"
+  "CMakeFiles/Chordmata_en_US.ts_lst_file"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chordmata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
